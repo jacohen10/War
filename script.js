13e4de6142a33da6435768da2fe6cb1405d98aaf
Josh Cohen <jacohen10@EMAIL.COM>
2015-10-28 09:26:07 -0400

    added comments in code

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ for (i=51; i>=((myDeck.length)/2); i--) {
   player2.push(myDeck[i]);
 }
 
-// here I need to take the card from the top of each players' deck and compare their values
+// here I take index 0 of each player's deck and put them into separate arrays so their values can be compared later
 
 var player1Hand = [];
 var player2Hand = [];
@@ -60,6 +60,7 @@ function play() {
   seeHand(player2Hand);
   colorCardsRed();
 }
+
 
 function colorCardsRed(){
   for (i=0; i<$(".card").length; i++) {
@@ -90,7 +91,7 @@ function colorWarCardsRed(){
 }
 
 
-// adding html so you can see the hand being played
+// adding html so you can see the special characters of the suits
 function seeHand(player) {
 	for(var i=0; i < player.length; i++){
 		newDiv = document.createElement('div');
@@ -125,7 +126,6 @@ function seeWarHand(player, x) {
 }
 
 function determineWinner() {
-
   console.log(player1Name,"played", player1Hand[player1Hand.length-1].name, "of", player1Hand[player1Hand.length-1].suit);
   console.log(player2Name,"played", player2Hand[player2Hand.length-1].name, "of", player2Hand[player2Hand.length-1].suit);
   if (player1Hand[player1Hand.length-1].value > player2Hand[player2Hand.length-1].value) {
@@ -154,6 +154,7 @@ function determineWinner() {
   }
 }
 
+// moves everything in the 2 arrays that are used for the current hand and adds them to the winner's main array
 function winnerTakesCards(player){
   Array.prototype.push.apply(player, player1Hand);
   Array.prototype.push.apply(player, player2Hand);
@@ -165,6 +166,7 @@ function winnerNotification(player) {
   newDiv.innerHTML = "<p>"+ player + " wins!</p>";
   $("header").append(newDiv);
 }
+
 
 function resetHand() {
   player1Hand = [];
@@ -179,7 +181,6 @@ $("button").eq(1).on("click", function() {
 });
 
 // add player names
-
 var player1Name = "Player 1";
 var player2Name = "Player 2";
 
@@ -192,11 +193,13 @@ $("button").eq(0).on("click", function () {
   $(".score2").text(player2Name + "'s Cards remaining: " + player2.length);
 });
 
+// moved them so they were easier to target for css
 function moveWarCards() {
   $(".topHalf").append($('[id*="warCards1"]'));
   $(".bottomHalf").append($('[id*="warCards2"]'));
 }
 
+// adds css to the last card for each player when there's war so it stands out
 function lastWarCards() {
   $('[id*="warCards1"]')[$('[id*="warCards1"]').length-1].style.height = "175px";
   $('[id*="warCards1"]')[$('[id*="warCards1"]').length-1].style.width = "125px";
@@ -208,6 +211,7 @@ function lastWarCards() {
   $('[id*="warCards2"]')[$('[id*="warCards2"]').length-1].style.fontSize = "55px";
 }
 
+// forces war(a tie) to happen
 function test() {
   player1[0].value = 10;
   player2[0].value = 10;
